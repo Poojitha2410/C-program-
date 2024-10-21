5121eaeafb6c1edb159913ddd1a309bb5aeb4b27
Poojitha2410 <doosanapudipoojitha@gmail.com>
2024-10-21 12:13:47 +0530

    Create vowelornot.c

---
+++ vowelornot.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    char p;
+    printf("enter an alphabet");
+    scanf("%c",&p);
+    if(p=='a'|| p=='e'|| p=='i'|| p=='o'|| p=='u')
+    {
+        printf("it is vowel");
+    }
+    else
+    {
+        printf("it is not an vowel");
+        }
+    }
